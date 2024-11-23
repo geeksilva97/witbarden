724da4068649696eef217b5c2c7f601acf11fbd6
Edy Silva <edigleyssonsilva@gmail.com>
2024-11-23 13:52:57 -0300

    add a few pages

---
+++ src/routes/(admin)/+page.svelte
@@ -0,0 +1,29 @@
+<div class="overflow-x-auto">
+    <div class="w-full flex justify-end">
+        <button type="submit" class="flex justify-center rounded-md bg-indigo-600 mb-2 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            New Vault
+        </button>
+    </div>
+
+    <table class="min-w-full table-auto">
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="px-4 py-2 text-left">Name</th>
+                <th class="px-4 py-2 text-left">Username</th>
+                <th class="px-4 py-2 text-left">Password</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-t">
+                <td class="px-4 py-2">John Doe</td>
+                <td class="px-4 py-2">john@gmail.com</td>
+                <td class="px-4 py-2">
+                    <div class="flex items-center gap-x-1">
+                        <span> *** </span>
+                        <button type="submit" class="outline rounded-md px-1.5 py-1 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Copy</button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
